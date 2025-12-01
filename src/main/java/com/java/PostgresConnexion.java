5aac79536370f034f1db249ec7411119f38b0717
Ranto <hei.ranto.3@gmail.com>
2025-12-01 15:46:04 +0300

    fix: postgresconnexion class

--- src/main/java/com/java/PostgresConnexion.java
+++ src/main/java/com/java/PostgresConnexion.java
@@ -1,4 +1,40 @@
 package com.java;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class PostgresConnexion {
+
+    private String url;
+    private String user;
+    private String password;
+
+    public PostgresConnexion(String url, String user, String password) {
+        this.url = url;
+        this.user = user;
+        this.password = password;
+    }
+
+    public Connection getConnection() {
+        try {
+            return DriverManager.getConnection(url, user, password);
+        } catch (SQLException e) {
+            System.out.println("Connexion error : " + e.getMessage());
+            return null;
+        }
+    }
+
+    public static void main(String[] args) {
+        PostgresConnexion pc = new PostgresConnexion(
+                "jdbc:postgresql://localhost:5432/jdbc",
+                "postgres",
+                "1234"
+        );
+
+        Connection conn = pc.getConnection();
+        if (conn != null) {
+            System.out.println("Connexion done !");
+        }
+    }
 }
